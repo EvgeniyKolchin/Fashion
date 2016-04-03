c7f7ee3135bbaa5307b1810b768233b39c1a11f6
Evgeniy Kolchin <zhenya1994@list.ru>
2016-04-03 20:58:22 +0300

    rewrite

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var db = new sqlite.Database('db/mydb.sqlite');
 
 app.use(express.static(__dirname + '/resource'));
 
+/*-----------------------------------------------------------------------------------------------------*/
 app.get('/', function(req, res) {
     db.all('select * from mydata', function(error, data){
         res.render('index', {
@@ -14,6 +15,21 @@ app.get('/', function(req, res) {
     });
 });
 
-app.listen(3000, function(){
-    console.log('listening on port 3000');
+app.post('/delete', function(req, res){
+    db.run("delete from mydata where id = "+req.query.id, function(){
+        res.send("ok");
+    })
+});
+
+app.post('/new', function(req, res){
+    db.run("insert into mydata (img_path, price, watch1) values('"+req.query.img_path+"','"+req.query.price+"','"+req.query.watch1+"')", function(){
+        res.send("ok");
+    })
+});
+
+/*-----------------------------------------------------------------------------------------------------*/
+var PORT = process.env.PORT || 3000;
+
+app.listen(PORT, function () {
+  console.log('Listening on port ' + PORT + '!');
 });
